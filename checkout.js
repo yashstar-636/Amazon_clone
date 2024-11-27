c89fe742866a97792911090c736248f91deda524
yashstar-636 <rathodyash636@gmail.com>
2024-11-27 20:54:21 +0530

    fixed radio formating

--- checkout.js
+++ checkout.js
@@ -26,7 +26,7 @@ cartSummaryHTML += `<div class="cart-item-container">
                 ${matchingProduct.name}
             </div>
             <div class="product-price">
-                ${matchingProduct.price}
+                ${(matchingProduct.price / 100).toFixed(2)}
             </div>
             <div class="product-quantity">
                 <span>
@@ -48,7 +48,7 @@ cartSummaryHTML += `<div class="cart-item-container">
             <div class="delivery-option">
                 <input type="radio" checked
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Tuesday, June 21
@@ -61,7 +61,7 @@ cartSummaryHTML += `<div class="cart-item-container">
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Wednesday, June 15
@@ -74,7 +74,7 @@ cartSummaryHTML += `<div class="cart-item-container">
             <div class="delivery-option">
                 <input type="radio"
                 class="delivery-option-input"
-                name="delivery-option-1">
+                name="delivery-option-${matchingProduct.id}">
                 <div>
                 <div class="delivery-option-date">
                     Monday, June 13
