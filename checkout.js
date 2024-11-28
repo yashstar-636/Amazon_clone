cad09d912aa3c8fe7652222d7e954ddc36dfafdf
yashstar-636 <rathodyash636@gmail.com>
2024-11-28 21:23:21 +0530

    delet products from html

--- checkout.js
+++ checkout.js
@@ -12,7 +12,7 @@ cart.forEach((cartItem) => {
  });
 
 
-cartSummaryHTML += `<div class="cart-item-container">
+cartSummaryHTML += `<div class="cart-item-container js-cart-item-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -168,5 +168,7 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     link.addEventListener('click',()=>{
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        const container=document.querySelector(`.js-cart-item-${productId}`);
+        container.remove();
     });
-})
+}) 
