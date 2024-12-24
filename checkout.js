f7dfcee32c2051ea0d30bc1aad9bfe647913e54e
yashstar-636 <rathodyash636@gmail.com>
2024-12-24 21:05:09 +0530

    add backend practice file

--- checkout.js
+++ checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import "./data/cart-classs.js"
+import './data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
--- data/products.js
+++ data/products.js
@@ -52,6 +52,7 @@ extraInfoHTML(){
 }
 
 
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
